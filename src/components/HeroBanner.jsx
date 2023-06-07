567463463bbec20780e2dd9a89d44f09eb06a4d2
yıldız <mesel4461@gmail.com>
2023-06-07 12:26:10 +0300

    as

---
+++ src/components/HeroBanner.jsx
@@ -0,0 +1,25 @@
+import { Box, Typography } from "@mui/material";
+import React from "react";
+
+const HeroBanner = () => {
+  return (
+    <Box
+      sx={{ mt: { lg: "212px", xs: "70px" }, ml: { sm: "50px" } }}
+      position="relative"
+      p="20px"
+    >
+      <Typography color="#FF2625" fontWeight="600" fontSize="26px">
+        Fitness Club
+      </Typography>
+      <Typography
+        fontWeight={700}
+        sx={{ fontSize: { lg: "44px", xs: "40px" } }}
+      >
+        Sweat,Smile <br /> and Repeat
+      </Typography>
+      <Typography>Check out the most effective exercises</Typography>
+    </Box>
+  );
+};
+
+export default HeroBanner;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,16 @@ import Logo from "../assets/images/Logo.png";
 
 const Navbar = () => {
   return (
-    <Stack>
+    <Stack
+      direction="row"
+      justifyContent="space-around"
+      sx={{
+        gap: { sm: "122px", xs: "40px" },
+        mt: { sm: "32px", xs: "20px" },
+        justifyContent: "none",
+        px: "20px",
+      }}
+    >
       <Link to="/">
         <img
           src={Logo}
@@ -17,12 +26,26 @@ const Navbar = () => {
           }}
         />
       </Link>
-      <Stack>
-        <Link to="/">Home</Link>
+      <Stack direction="row" gap="40px" fontSize="24px" alignItems="flex-end">
+        <Link
+          to="/"
+          style={{
+            textDecoration: "none",
+            color: "#3A1212",
+            borderBottom: "3px solid #FF2625",
+          }}
+        >
+          Home
+        </Link>
         <a
           href="#exercises"
-          style={{ textDecoration: "none", color: "#3A1212" }}
-        ></a>
+          style={{
+            textDecoration: "none",
+            color: "#3A1212",
+          }}
+        >
+          Exercises
+        </a>
       </Stack>
     </Stack>
   );
