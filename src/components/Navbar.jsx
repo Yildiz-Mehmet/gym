41762afa91df8a181180c2ca1c72481de7d48dc1
yıldız <mesel4461@gmail.com>
2023-06-07 11:11:46 +0300

    as

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Stack } from "@mui/material";
+import Logo from "../assets/images/Logo.png";
 
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <Stack>
+      <Link to="/">
+        <img
+          src={Logo}
+          alt="Logo"
+          style={{
+            width: "48px",
+            height: "48px",
+            margin: "0 20px",
+          }}
+        />
+      </Link>
+      <Stack>
+        <Link to="/">Home</Link>
+        <a
+          href="#exercises"
+          style={{ textDecoration: "none", color: "#3A1212" }}
+        ></a>
+      </Stack>
+    </Stack>
+  );
 };
 
 export default Navbar;
