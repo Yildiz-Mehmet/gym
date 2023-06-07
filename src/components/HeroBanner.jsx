83ebc14cca99e5540096dcd92b1a0b2c96aab598
yıldız <mesel4461@gmail.com>
2023-06-07 16:54:13 +0300

    as

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,5 +1,6 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import React from "react";
+import HeroBannerImage from "../assets/images/banner.png";
 
 const HeroBanner = () => {
   return (
@@ -14,10 +15,34 @@ const HeroBanner = () => {
       <Typography
         fontWeight={700}
         sx={{ fontSize: { lg: "44px", xs: "40px" } }}
+        mb="23px"
+        mt="30px"
       >
         Sweat,Smile <br /> and Repeat
       </Typography>
-      <Typography>Check out the most effective exercises</Typography>
+      <Typography fontSize="22px" lineHeight="35px" mb={4}>
+        Check out the most effective exercises
+      </Typography>
+      <Button
+        variant="contained"
+        color="error"
+        sx={{ backgroundColor: "#ff2625", padding: "10px" }}
+        href="#exercises"
+      >
+        Explore Exercises
+      </Button>
+      <Typography
+        fontWeight={600}
+        color="#ff2625"
+        sx={{
+          opacity: 0.1,
+          display: { lg: "block", xs: "none" },
+        }}
+        fontSize="200px"
+      >
+        Exercise
+      </Typography>
+      <img src={HeroBannerImage} alt="banner" className="hero-banner-img" />
     </Box>
   );
 };
