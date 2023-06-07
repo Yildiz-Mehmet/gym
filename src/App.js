a28d1d923d008beb995c1183c8814b3b28b388e9
yıldız <mesel4461@gmail.com>
2023-06-07 10:39:34 +0300

    as

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Box width="400px">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Routes>
+    </Box>
+  );
 };
 
 export default App;
