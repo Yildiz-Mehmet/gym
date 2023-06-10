7c61a0c9623b22e7dec460bea1fd1eb8ddc889a8
yıldız <mesel4461@gmail.com>
2023-06-10 12:08:02 +0300

    as

--- src/pages/ExerciseDetail.jsx
+++ src/pages/ExerciseDetail.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import { Box } from "@mui/material";
+import React, { useEffect, useLayoutEffect, useState } from "react";
+import Detail from "../components/Detail";
+import ExerciseVideos from "../components/ExerciseVideos";
+import SimilarExercises from "../components/SimilarExercises";
+import { useParams } from "react-router-dom";
 
 const ExerciseDetail = () => {
-  return <div>ExerciseDetail</div>;
+  const [exerciseDetail, setExerciseDetail] = useState({});
+  const { id } = useParams();
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const exerciseDbUrl = "exercisedb.p.rapidapi.com";
+    };
+    fetchExercisesData();
+  }, [id]);
+
+  return (
+    <Box>
+      <Detail exerciseDetail={exerciseDetail} />
+      <ExerciseVideos />
+      <SimilarExercises />
+    </Box>
+  );
 };
 
 export default ExerciseDetail;
