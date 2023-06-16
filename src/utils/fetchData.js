390dcc25b137448968e3522fce4de0bf97408885
yıldız <mesel4461@gmail.com>
2023-06-16 20:52:58 +0300

    as

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -8,6 +8,13 @@ export const exerciseOptions = {
     "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
   },
 };
+export const youtubeOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
+    "X-RapidAPI-Key": "df3de8d2d8mshf2cfe1379b74260p1a5fbejsn135ceadaa208",
+  },
+};
 
 export const fetchData = async (url, options) => {
   const response = await fetch(url, options);
